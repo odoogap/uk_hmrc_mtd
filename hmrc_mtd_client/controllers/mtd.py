4d4bb1842a305f0c24f95ba83ee471a434ccb604
mfigueiredo <mfigueiredo@odoogap.com>
2019-02-18 13:04:35 +0000

    [FIX] fix hmrc message

--- hmrc_mtd_client/controllers/mtd.py
+++ hmrc_mtd_client/controllers/mtd.py
@@ -13,17 +13,9 @@ class Mtd(http.Controller):
 
     @http.route('/mtd/<string:token>/<string:ex_date>', auth='user')
     def redirect(self, **kw):
-        web_url = request.env['ir.config_parameter'].get_param('web.base.url')
         if kw.get('token') != 'Invalid':
             request.env['ir.config_parameter'].sudo().set_param('mtd.token', kw.get('token'))
             request.env['ir.config_parameter'].sudo().set_param('mtd.token_expire_date', float(kw.get('ex_date')))
-            webbrowser.open_new(web_url + '/mtd/message/success')
-        else:
-            webbrowser.open_new(web_url + '/mtd/message/failure')
-
-    @http.route('/mtd/message/<string:message>', auth='user')
-    def redirect_message(self, **kw):
-        if kw.get('message') == 'success':
             return 'Success! you can close the HMRC Page'
         else:
             return 'Failed! Something went wrong'
