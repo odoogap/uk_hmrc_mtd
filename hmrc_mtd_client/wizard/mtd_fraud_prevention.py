61715396ae84cbc72968620b316a825c8d8716c8
Fábio Oliveira <foliveira@odoogap.com>
2021-01-18 16:17:00 +0000

    [IMP] update headers and add requirement

--- hmrc_mtd_client/wizard/mtd_fraud_prevention.py
+++ hmrc_mtd_client/wizard/mtd_fraud_prevention.py
@@ -4,12 +4,11 @@
 #    __manifest__.py file at the root folder of this module.                  #
 ###############################################################################
 
-from odoo import models, fields, api, _
-from requests import get
 import uuid
-from datetime import datetime, timezone
+import odoogap_mtd as mtd
+
+from odoo import models, fields, api, _
 from odoo.http import request
-import socket
 
 
 class MtdFraudPrevention(models.TransientModel):
@@ -48,22 +47,11 @@ class MtdFraudPrevention(models.TransientModel):
                 'browser_plugin': data.get('browser_plugin', False)
             })
 
-    def get_local_ip(self):
-        return ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
-                              if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
-                                                                    s.getsockname()[0], s.close()) for s in
-                                                                   [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][
-                                                                      0][1]]) if l][0][0])
-
     def create_fraud_prevention_headers(self):
-        public_ip = get('https://api.ipify.org').text
-        public_vendor_ip = socket.gethostbyname('odoomtd.co.uk')
         params = self.env['ir.config_parameter'].sudo()
         gov_device_id = params.get_param('mtd.gov_device_id', default=False)
-        date_string = str(datetime.now(timezone.utc).astimezone().isoformat())
         user = self.env['res.users'].sudo().browse(self.env.uid)
         login_date_format = user.login_date.strftime("%Y/%m/%d, %H:%M")
-        timestamp = login_date_format.replace("/", "-").replace(", ", "T").replace(":", "%3A") + "Z"
         unique_reference = user.company_id.id
         module_version = self.env['ir.module.module'].search([('name', '=', 'hmrc_mtd_client')]).installed_version
         licence_ids = self.env['ir.module.module'].search([('name', '=', 'hmrc_mtd_client')]).license
@@ -71,31 +59,11 @@ class MtdFraudPrevention(models.TransientModel):
         if not gov_device_id:
             gov_device_id = self.generate_device_id()
 
-        if '+' in date_string:
-            data = date_string.rsplit('+', 1)
-            utc_time = "UTC+%s" % str(data[1])
-        else:
-            data = date_string.rsplit('-', 1)
-            utc_time = "UTC-%s" % str(data[1])
-
         record = self.search([('user_id', '=', self.env.user.id)], limit=1)
 
-        return {
-            'Gov-Client-Connection-Method': 'WEB_APP_VIA_SERVER',
-            'Gov-Client-Public-IP': public_ip,
-            'Gov-Client-Public-Port': request.httprequest.environ.get('SERVER_PORT'),
-            'Gov-Client-Device-ID': gov_device_id,
-            'Gov-Client-User-IDs': 'My_Webapp_Software=' + str(self.env.user.id),
-            'Gov-Client-Timezone': utc_time,
-            'Gov-Client-Local-IPs': self.get_local_ip(),
-            'Gov-Client-Screens': record.screens if record else "width=1920&height=1080&scaling-factor=1.7777777777777777&colour-depth=24",
-            'Gov-Client-Window-Size': record.window_size if record else "width=1920&height=1080",
-            'Gov-Client-Browser-Plugins': record.browser_plugin if record else 'Native%20Client',
-            'Gov-Client-Browser-JS-User-Agent': record.js_user_agent if record else "",
-            'Gov-Client-Browser-Do-Not-Track': "false",
-            'Gov-Client-Multi-Factor': "type=OTHER&timestamp=" + timestamp + "&unique-reference=" + str(hash(unique_reference)),
-            'Gov-Vendor-Version': "hmrc_mtd_client" + "=" + str(module_version) + "&hmrc_mtd_server" + "=" + "0.1",
-            'Gov-Vendor-License-IDs': "hmrc_mtd_server" + "=" + str(hash(licence_ids)),
-            'Gov-Vendor-Public-IP': public_vendor_ip,
-            'Gov-Vendor-Forwarded': "by=" + public_vendor_ip + "&for=" + public_ip
-        }
+        return mtd.mtd_headers.create_headers_dic(request.httprequest.environ.get('SERVER_PORT'), gov_device_id,
+                                                  user.id,
+                                                  record.screens, record.window_size, record.browser_plugin,
+                                                  record.js_user_agent, login_date_format, unique_reference,
+                                                  module_version,
+                                                  licence_ids)
