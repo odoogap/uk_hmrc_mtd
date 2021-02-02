240c6a9d59473f2bed6895b60543b2e97a95bac6
Fábio Oliveira <foliveira@odoogap.com>
2021-02-02 11:11:37 +0000

    [IMP] new changes in vat boxes

--- hmrc_mtd_client/__manifest__.py
+++ hmrc_mtd_client/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "hmrc_mtd_client",
-    'version': '1.1.8',
+    'version': '1.1.9',
     'summary': """Client module for management of HMRC""",
     'description': """
         Enables the user to commit HMRC VAT return to HMRC api.
