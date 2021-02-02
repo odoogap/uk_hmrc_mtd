9cb326dd55ee95e67568272d6d932c94388add8d
Fábio Oliveira <foliveira@odoogap.com>
2021-02-02 11:31:28 +0000

    [IMP] new changes in vat boxes

--- hmrc_mtd_client/__manifest__.py
+++ hmrc_mtd_client/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "hmrc_mtd_client",
-    'version': '1.1.7',
+    'version': '1.1.9',
     'summary': """Client module for management of HMRC""",
     'description': """
         Enables the user to commit HMRC VAT return to HMRC api.
--- hmrc_mtd_client/wizard/mtd_vat_formula.py
+++ hmrc_mtd_client/wizard/mtd_vat_formula.py
@@ -25,7 +25,7 @@ class MtdCalculationFormula(models.TransientModel):
     def get_default_params_formula(self, fields):
         IrConfigParam = self.env['ir.config_parameter']
         return {
-            'box_one': IrConfigParam.get_param('mtd.box_one_formula', 'sum([vat_ST0,vat_ST1,vat_ST2,vat_ST11]) + vat_fuel + vat_bad'),
+            'box_one': IrConfigParam.get_param('mtd.box_one_formula', 'sum([vat_ST1,vat_ST2,vat_ST11]) + vat_fuel + vat_bad'),
             'box_two': IrConfigParam.get_param('mtd.box_two_formula', 'sum([vat_PT8M])'),
             'box_four': IrConfigParam.get_param('mtd.box_four_formula', 'sum([vat_PT11,vat_PT5,vat_PT2,vat_PT1,vat_PT0]) + sum([vat_credit_PT8R,vat_debit_PT8R])'),
             'box_six': IrConfigParam.get_param('mtd.box_six_formula', 'sum([net_ST0,net_ST1,net_ST2,net_ST11]) + sum([net_ST4]) + net_fuel + net_bad'),
